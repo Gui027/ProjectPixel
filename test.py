adc29a4445f404a0fbcfeaa6b7e5462914323278
Gui027 <guilhermerg1234@gmail.com>
2023-12-18 21:55:15 -0300

    Salvando arquivo na área de trabalho e fechando após executar a api

--- test.py
+++ test.py
@@ -1,24 +1,47 @@
+import os
 import requests
 import csv
+import tkinter as tk
+from tkinter import messagebox
 
-# Endereço da API
-url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+def executar_codigo():
+    # Endereço da API
+    url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 
-# Chamada à API
-resposta = requests.get(url)
+    # Chamada à API
+    resposta = requests.get(url)
 
-# Decodificação da resposta
-dados = resposta.json()
+    # Decodificação da resposta
+    dados = resposta.json()
 
-# Criação do arquivo CSV
-arquivo = open("dados.csv", "w", newline="")
+    try:
+        # Obter o caminho da área de trabalho
+        desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
 
-# Escrita dos dados no arquivo CSV
-escritor = csv.writer(arquivo)
-escritor.writerow(["success", "deck_id", "remaining", "shuffled"])
+        # Criação do arquivo CSV na área de trabalho
+        with open(os.path.join(desktop_path, "dados.csv"), "w", newline="") as arquivo:
+            # Escrita dos dados no arquivo CSV
+            escritor = csv.writer(arquivo)
+            escritor.writerow(["success", "deck_id", "remaining", "shuffled"])
 
-# Adicionando dados ao arquivo CSV
-escritor.writerow([dados["success"], dados["deck_id"], dados["remaining"], dados["shuffled"]])
+            # Adicionando dados ao arquivo CSV
+            escritor.writerow([dados["success"], dados["deck_id"], dados["remaining"], dados["shuffled"]])
 
-# Fechamento do arquivo CSV
-arquivo.close()
+        messagebox.showinfo("Concluído", "Código executado com sucesso! Dados salvos em dados.csv na área de trabalho")
+
+        # Fechar a janela após a mensagem de sucesso
+        root.destroy()
+
+    except Exception as e:
+        messagebox.showerror("Erro", f"Ocorreu um erro: {str(e)}")
+
+# Configuração da interface gráfica
+root = tk.Tk()
+root.title("Executar Código")
+
+# Botão para executar o código
+botao_executar = tk.Button(root, text="Executar Código", command=executar_codigo)
+botao_executar.pack(pady=20)
+
+# Iniciar o loop da interface gráfica
+root.mainloop()
